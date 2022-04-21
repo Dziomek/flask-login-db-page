6ad4a799e21f3fedaff50ca12f4a2ed9d0bd904b
Dominik <company.com>
2022-04-21 22:27:23 +0200

    DB models

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'ABC'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app) # łączenie bazy danych z aplikacją
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/') # informacja dla app jak dotrzec do blueprintow
+    app.register_blueprint(views, url_prefix='/')  # informacja dla app jak dotrzec do blueprintow
     app.register_blueprint(auth, url_prefix='/')
 
     return app
