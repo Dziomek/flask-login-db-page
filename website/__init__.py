adbec05c1def4bb555d8a2f9b7746131b1f0ff17
Dominik <company.com>
2022-04-21 21:09:17 +0200

    Add forms, login and register

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = ''
+    app.config['SECRET KEY'] = 'secret key'
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)  # Tworzenie blueprinta
 
 
-@auth.route('/')  # poniższa funkcja odpali się, gdy wejdziemy w stonę [ROUTE_Z_BLUEPRINTA_W_APP/[ROUTE] w tym przypadku /auth/
+@auth.route('/')
 def home():
     return "<h1>Test</h1>"
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST']) # aby dekorator obsługiwał żądania GET i POST
 def login():
-    return render_template("login.html", name='Dominik', boolean=False)
+   return render_template("login.html") #name='Dominik', boolean=False)
 
 
 @auth.route('/logout')
@@ -18,6 +18,23 @@ def logout():
     return 'Logout'
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email length must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name length must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            flash('Successfully registered', category='success')
+
     return render_template("sign_up.html")
