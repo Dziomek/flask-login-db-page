4e2d6b9eaf486a9a5fa8f75c5e6a520f9d2d1473
Dziomek <dziomek@student.agh.edu.pl>
2022-04-22 16:27:04 +0200

    Delete notes

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, flash
 from .models import Note
 from flask_login import login_user, login_required, logout_user, current_user
 from . import db
+import json
+import jsonify
 
 # Blueprinty zawierają w sobie routy
 
@@ -24,3 +26,14 @@ def home():
 
     return render_template("home.html", user=current_user)
 
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
